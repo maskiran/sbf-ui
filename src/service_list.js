255ebe43cb1f57aacdeb64b12385ea4907309f7d
Kiran Madabhushi <maskiran@gmail.com>
2020-02-13 22:37:44 -0800

    refactor code to handle manual browser url changes

--- src/service_list.js
+++ src/service_list.js
@@ -9,19 +9,27 @@ class ServiceList extends React.Component {
     constructor(props) {
         super(props);
         // parse the search query params and assign to the state
-        var queryParams = qs.parse(props.location.search);
         // name is update when the user presses 'enter' on the search
         // box. searchValue maintains the current value (as being typed)
         // in the search box
         this.state = {
             services: {},
-            searchValue: queryParams.search || null,
+            searchValue: "",
             addServiceFormVisible: false,
             selectedRows: [],
             editorValues: {},
         }
         this.defaultPage = 1;
         this.defaultPageSize = 25;
+        this.searchParams = {};
+        this.parseAndSetSearchParams();
+    }
+
+    parseAndSetSearchParams = () => {
+        // from the url args (received via props), set searchParams.
+        // this is used by paginator to update the values and triggers
+        // the component update
+        var queryParams = qs.parse(this.props.location.search);
         this.searchParams = {
             search: queryParams.search || null,
             page: queryParams.page || this.defaultPage,
@@ -29,56 +37,134 @@ class ServiceList extends React.Component {
         }
     }
 
-    columns = [
-        {
-            title: 'Idx',
-            key: 'idx',
-            className: 'td-fit',
-            render: (text, record, index) => {
-                return (this.state.services.start_idx + index + 1)
-            }
-        },
-        {
-            title: 'Name',
-            dataIndex: 'name',
-            render: (text) => {
-                var url = "/service/" + text + "/home";
-                return <Link to={url}>{text}</Link>
-            }
-        },
-        {
-            title: 'Listen Port',
-            dataIndex: 'listen_port'
-        },
-        {
-            title: 'Target',
-            dataIndex: 'target'
-        },
-        {
-            title: 'WAF',
-            dataIndex: 'waf_profile'
-        },
-        {
-            title: 'TLS',
-            dataIndex: 'tls_profile'
-        },
-        {
-            title: 'Added On',
-            dataIndex: 'date_added',
-            className: 'td-fit',
-            render: (text) => {
-                return new Date(text).toLocaleString()
-            }
-        },
-        {
-            title: 'Modified On',
-            dataIndex: 'date_modified',
-            className: 'td-fit',
-            render: (text) => {
-                return new Date(text).toLocaleString()
-            }
-        },
-    ]
+    componentDidMount() {
+        this.getServices();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        this.parseAndSetSearchParams();
+        var oldUrl = prevProps.location.pathname + prevProps.location.search;
+        var newUrl = this.makeBrowserUrl();
+        if (newUrl !== oldUrl) {
+            this.getServices();
+        }
+    }
+
+    render() {
+        return (
+            <div style={{ padding: "25px" }}>
+                {this.renderActionsRow()}
+                {this.renderServicesListTable()}
+                {this.renderAddNewServiceModal()}
+            </div>
+        )
+    }
+
+    renderAddNewServiceModal = () => {
+        return (
+            <Modal visible={this.state.addServiceFormVisible} title="Add Service"
+                width={"40%"}
+                onCancel={this.hideAddServiceForm}
+                onOk={this.addService}>
+                <ServiceEditForm onChange={this.updateEditorValues} createMode={true}
+                    editorValues={this.state.editorValues} />
+            </Modal>
+        )
+    }
+
+    renderActionsRow = () => {
+        return (
+            <Row style={{marginBottom: "15px"}}>
+                <Col span={18}>
+                    <Button type="primary"
+                        onClick={this.showAddServiceForm}>Add Service</Button>
+                    <Button type="danger" style={{ marginLeft: "10px" }}
+                        onClick={this.deleteService}
+                        disabled={!this.state.selectedRows.length}>Delete</Button>
+                </Col>
+                <Col span={6}>
+                    <Input.Search
+                        name="searchValue"
+                        value={this.state.searchValue}
+                        onSearch={this.handleOnSearch}
+                        onChange={this.handleInputChange} />
+                </Col>
+            </Row>
+        )
+    }
+
+    renderServicesListTable = () => {
+        return (
+            <Table dataSource={this.state.services.items}
+                columns={this.getTableColumns()} size="middle" bordered
+                rowKey="name"
+                rowSelection={{
+                    onChange: this.handleRowSelection
+                }}
+                pagination={{
+                    pageSize: this.state.services.page_size || 10,
+                    total: this.state.services.count,
+                    current: this.state.services.page,
+                    onChange: this.handlePagerClick
+                }}
+            />
+        )
+    }
+
+    getTableColumns = () => {
+        var columns = [
+            {
+                title: 'Idx',
+                key: 'idx',
+                className: 'td-fit',
+                render: (text, record, index) => {
+                    return (this.state.services.start_idx + index + 1)
+                }
+            },
+            {
+                title: 'Name',
+                dataIndex: 'name',
+                render: (text) => {
+                    var url = "/service/" + text + "/home";
+                    return <Link to={url}>{text}</Link>
+                }
+            },
+            {
+                title: 'Listen Port',
+                dataIndex: 'listen_port'
+            },
+            {
+                title: 'Target',
+                dataIndex: 'target'
+            },
+            {
+                title: 'WAF',
+                dataIndex: 'waf_profile'
+            },
+            {
+                title: 'TLS',
+                dataIndex: 'tls_profile'
+            },
+            {
+                title: 'Added On',
+                dataIndex: 'date_added',
+                className: 'td-fit',
+                render: (text) => {
+                    return new Date(text).toLocaleString()
+                }
+            },
+            {
+                title: 'Modified On',
+                dataIndex: 'date_modified',
+                className: 'td-fit',
+                render: (text) => {
+                    return new Date(text).toLocaleString()
+                }
+            },
+        ]
+
+        return columns
+    }
 
     handlePagerClick = (page, pageSize) => {
         this.searchParams.page = page;
@@ -98,11 +184,15 @@ class ServiceList extends React.Component {
         this.setState({ [e.target.name]: e.target.value })
     }
 
-    handleRowSelection = (selectedKeys, records) => {
+    handleRowSelection = (selectedKeys) => {
         this.setState({ selectedRows: selectedKeys })
     }
 
     showAddServiceForm = () => {
+        this.setState({ addServiceFormVisible: true })
+    }
+
+    hideAddServiceForm = () => {
         this.setState({ addServiceFormVisible: true })
     }
 
@@ -125,7 +215,7 @@ class ServiceList extends React.Component {
             promises.push(promise);
         }
         if (promises.length) {
-            this.setState({selectedRows: []});
+            this.setState({ selectedRows: [] });
             axios.all(promises).then(rspList => {
                 this.getServices();
             })
@@ -160,67 +250,6 @@ class ServiceList extends React.Component {
         this.props.history.push(url);
     }
 
-    componentDidMount() {
-        this.getServices();
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        var oldUrl = prevProps.location.pathname + prevProps.location.search;
-        var newUrl = this.makeBrowserUrl();
-        if (newUrl !== oldUrl) {
-            this.getServices();
-        }
-    }
-
-    renderActionsRow = () => {
-        return (
-            <Row>
-                <Col span={18}>
-                    <Button type="primary"
-                        onClick={this.showAddServiceForm}>Add Service</Button>
-                    <Button type="danger" style={{marginLeft: "10px"}}
-                        onClick={this.deleteService}
-                        disabled={!this.state.selectedRows.length}>Delete</Button>
-                </Col>
-                <Col span={6}>
-                    <Input.Search
-                        name="searchValue"
-                        value={this.state.searchValue}
-                        onSearch={this.handleOnSearch}
-                        onChange={this.handleInputChange} />
-                </Col>
-            </Row>
-        )
-    }
-
-    render() {
-        return (
-            <div style={{ padding: "25px" }}>
-                <Modal visible={this.state.addServiceFormVisible} title="Add Service"
-                    width={"40%"}
-                    onCancel={() => this.setState({ addServiceFormVisible: false })}
-                    onOk={this.addService}>
-                    <ServiceEditForm onChange={this.updateEditorValues} createMode={true}
-                        editorValues={this.state.editorValues} />
-                </Modal>
-                <div style={{ marginBottom: "10px" }}>
-                    {this.renderActionsRow()}
-                </div>
-                <Table dataSource={this.state.services.items}
-                    columns={this.columns} size="middle" bordered
-                    rowKey="name"
-                    rowSelection={{
-                        onChange: this.handleRowSelection
-                    }}
-                    pagination={{
-                        pageSize: this.state.services.page_size || 10,
-                        total: this.state.services.count,
-                        current: this.state.services.page,
-                        onChange: this.handlePagerClick
-                    }} />
-            </div>
-        )
-    }
 }
 
 export default ServiceList;
